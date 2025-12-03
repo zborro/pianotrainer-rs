d8fc771118b1370b879765245e16d41416fcf759
Sławomir Zborowski <slawek@zborro.dev>
2025-12-03 14:24:03 +0100

    use clap lib for handling cli params

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,25 @@
 use macroquad::prelude::*;
-use std::env;
-use std::path::Path;
+use std::path::PathBuf;
+use clap::Parser;
 
 mod song;
 mod screen;
 
+#[derive(Parser)]
+struct Cli {
+    midi_path: PathBuf,
+}
+
 #[macroquad::main("zborro-piano-trainer")]
 async fn main() {
-    if env::args().len() != 2 {
-        panic!("ERROR : wrong number of arguments! Expected file as arg");
+    let args = Cli::parse();
+    println!("{:?}", args.midi_path.to_str());
+
+    if !args.midi_path.exists() {
+        panic!("passed MIDI file does not exist!");
     }
 
-    let path_string = env::args().nth(1).unwrap();
-    let path = Path::new(&path_string);
-
-    let song = song::load_song(path);
+    let song = song::load_song(args.midi_path.as_path());
 
     let mut last_screen_width = screen_width();
     let piano_screen_handle = scene::add_node(screen::PianoScreen::new(song));
